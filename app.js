4359603edfb33a9a5e2d6794740e96e6938f992a
Jeferson <jeh.jim@gmail.com>
2022-09-21 15:12:40 -0300

    atualizando

--- app.js
+++ app.js
@@ -1,1 +1,5 @@
 console.log("Rodando o siste de gere de jogos")
+console.log("aplicação está se conectando ao banco de dados")
+// mais um console log de teste:
+console.log("O paulo fez esse commit sem voce ver na aula!")
+console.log("enviando para o github")
